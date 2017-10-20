76743beb36666916b52d9578eae3e1fb77ebb75b
jenazad <felipe.moreno.vera@gmail.com>
2017-10-19 19:09:08 -0500

    modify secret key

--- examples/goJwetoHandler.go
+++ examples/goJwetoHandler.go
@@ -9,7 +9,7 @@ import (
 // create a JWT and put in the clients cookie
 func setToken(w http.ResponseWriter, r *http.Request) {
   w.Header().Set("Content-Type", "application/json")
-  gojweto.SetSecretKey("beagons-web")
+  gojweto.SetSecretKey("Jnzads-web")
   tokenString, _ := gojweto.CreateHS256Token("localhost:9000")
   dataJSON := gojweto.Credentials{Token: tokenString, Logged: true}
   gojweto.JsonResponse(dataJSON, w)
