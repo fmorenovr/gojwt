92736eeaa01be2f1dc925e0060ce687961124198
Felipe A. Moreno <fmorenovr@gmail.com>
2018-03-22 14:46:51 -0500

    Update gojweto.go

--- gojweto.go
+++ gojweto.go
@@ -9,6 +9,7 @@ import (
   "github.com/dgrijalva/jwt-go";
 )
 
+// goJweto Struct
 type Gojweto struct {
   pubECDSAPath     string             // pub ECDSA path
   privECDSAPath    string             // priv ECDSA path
@@ -26,6 +27,7 @@ type Gojweto struct {
   nameServer       string             // claims info
 }
 
+// prepare RSA Key pairs function
 func prepareRSAKeys(privRSAPath, pubRSAPath string)(*rsa.PublicKey, *rsa.PrivateKey){
   pwd, _ := os.Getwd()
 
@@ -44,6 +46,7 @@ func prepareRSAKeys(privRSAPath, pubRSAPath string)(*rsa.PublicKey, *rsa.Private
   return verifiedKey, signedKey
 }
 
+// prepare ECDSA Key pairs function
 func prepareECDSAKeys(privECDSAPath, pubECDSAPath string)(*ecdsa.PublicKey, *ecdsa.PrivateKey){
   pwd, _ := os.Getwd()
 
@@ -62,6 +65,7 @@ func prepareECDSAKeys(privECDSAPath, pubECDSAPath string)(*ecdsa.PublicKey, *ecd
   return verifiedKey, signedKey
 }
 
+// Create a New GoJweto Instance with HMAC-SHA encrypt method by default
 func NewGojweto() (Gojweto){
 
   return Gojweto{
@@ -73,6 +77,7 @@ func NewGojweto() (Gojweto){
          nameServer: "JnzadsServer"}
 }
 
+// Create a New GoJweto Instance with an encrypt method with parameters as you wish
 func NewGojwetoOptions(privKeyPath, pubKeyPath, nameserver, secretkey, headerkey, method, bytes string, hours time.Duration) (Gojweto){
   var verifiedRSAKey   *rsa.PublicKey
   var signedRSAKey     *rsa.PrivateKey
@@ -117,93 +122,111 @@ func NewGojwetoOptions(privKeyPath, pubKeyPath, nameserver, secretkey, headerkey
   }
 }
 
-// hours of duration
+// set hours of token duration
 func (o *Gojweto) SetNumHoursDuration(hours time.Duration){
   o.numHoursDuration = hours
 }
 
+// get hours of token duration
 func (o *Gojweto) GetNumHoursDuration()(time.Duration){
   return o.numHoursDuration
 }
 
-// Header authorization
+// set Header authorization
 func (o *Gojweto) SetHeaderKey(name string){
   o.headerKeyAuth = name
 }
 
+// get Header authorization
 func (o *Gojweto) GetHeaderKey()(string){
   return o.headerKeyAuth
 }
 
-// Name Server
+// set Name Server
 func (o *Gojweto) SetNameServer(name string){
   o.nameServer = name
 }
 
+// get Name Server
 func (o *Gojweto) GetNameServer()(string){
   return o.nameServer
 }
 
 // just for method HMACSHA
+// set secret key word to encrypt using hmac-sha
 func (o *Gojweto) SetSecretKey(name string){
   o.secretKeyWord = name
 }
 
+// get secret key word to encrypt using hmac-sha
 func (o *Gojweto) GetSecretKey()(string){
   return o.secretKeyWord
 }
 
+// get secret key word convert in bytes
 func (o *Gojweto) GetSecretByte()([]byte){
   return []byte(o.secretKeyWord)
 }
 
 // just for method RSA
+// get RSA private key
 func (o *Gojweto) GetRSAPrivKey()(*rsa.PrivateKey){
   return o.privRSAKey
 }
 
+// get RSA public key
 func (o *Gojweto) GetRSAPubKey()(*rsa.PublicKey){
   return o.pubRSAKey
 }
 
 // just for method ECDSA
+// get ECDSA private key
 func (o *Gojweto) GetECDSAPrivKey()(*ecdsa.PrivateKey){
   return o.privECDSAKey
 }
 
+// get ECDSA public key
 func (o *Gojweto) GetECDSAPubKey()(*ecdsa.PublicKey){
   return o.pubECDSAKey
 }
 
 // path of keys RSA/ECDSA
+// set PATH of RSA Public key
 func (o *Gojweto) SetPubRSAPath(path string)(){
   o.pubRSAPath = path
 }
 
+// get PATH of RSA Public key
 func (o *Gojweto) GetPubRSAPath()(string){
   return o.pubRSAPath
 }
 
+// set PATH of RSA Private key
 func (o *Gojweto) SetPrivRSAPath(path string)(){
   o.privRSAPath = path
 }
 
+// get PATH of RSA Private key
 func (o *Gojweto) GetPrivRSAPath()(string){
   return o.privRSAPath
 }
 
+// set PATH of ECDSA Public key
 func (o *Gojweto) SetPubECDSAPath(path string)(){
   o.pubECDSAPath = path
 }
 
+// get PATH of ECDSA Public key
 func (o *Gojweto) GetPubECDSAPath()(string){
   return o.pubECDSAPath
 }
 
+// set PATH of ECDSA Private key
 func (o *Gojweto) SetPrivECDSAPath(path string)(){
   o.privECDSAPath = path
 }
 
+// get PATH of ECDSA Private key
 func (o *Gojweto) GetPrivECDSAPath()(string){
   return o.privECDSAPath
 }
@@ -213,15 +236,17 @@ func (o *Gojweto) SetEncryptMethod(method string)(){
   o.method = method
 }
 
+// get current encrypt method
 func (o *Gojweto) GetEncryptMethod()(string){
   return o.method
 }
 
-// change encrypt method
+// set bytes of encrypt method
 func (o *Gojweto) SetEncryptLenBytes(lenBytes string)(){
   o.lenBytes = lenBytes
 }
 
+// get bytes of encrypt method
 func (o *Gojweto) GetEncryptLenBytes()(string){
   return o.lenBytes
 }
