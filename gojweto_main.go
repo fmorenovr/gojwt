836bf980d95e5c859380c1568f51f1c5947d1047
Felipe A. Moreno <fmorenovr@gmail.com>
2018-03-23 12:02:34 -0500

    Update gojweto_main.go

--- gojweto_main.go
+++ gojweto_main.go
@@ -1,29 +1,27 @@
-package gojweto
+package gojweto_test
 
 import (
   "fmt";
-  //"github.com/jenazads/gojweto";
+  "github.com/jenazads/gojweto";
 )
 
-var (
-  privECDSAKeyPath = "asdsa"
-  pubECDSAKeyPath  = ""
-  privRSAKeyPath   = ""
-  pubRSAKeyPath    = ""
-)
-
+// main function with hello world in goJweto
 func main() {
-  //GojwtObject, err := gojweto.NewGojweto()
-  GojwtObject, err := NewGojweto()
+  var (
+    privECDSAKeyPath = "asdsa"
+    pubECDSAKeyPath  = ""
+    privRSAKeyPath   = ""
+    pubRSAKeyPath    = ""
+  )
+  GojwtObject, err := gojweto.NewGojweto()
   fmt.Println("Example with Default config GoJweto Object: ", GojwtObject, "with error: ", err)
   
-  //GojwtObject, err = gojweto.NewGojwetoOptions("", "", "", "", "Jnzads-rest-JWT", "HMAC-SHA", "512", 24)
-  GojwtObject, err = NewGojwetoOptions("", "", "", "", "Jnzads-rest-JWT", "HMAC-SHA", "512", 24)
+  GojwtObject, err = gojweto.NewGojwetoOptions("", "", "", "", "Jnzads-rest-JWT", "HMAC-SHA", "512", 24)
   fmt.Println("Example with empty secret Key and HMAC method: ", GojwtObject, "with error: ", err)
-  //GojwtObject, err = gojweto.NewGojwetoOptions(privECDSAKeyPath, pubECDSAKeyPath, "JnzadsServer", "jnzads-rest", "Jnzads-rest-JWT", "ECDSA", "384", 24)
-  GojwtObject, err = NewGojwetoOptions(privECDSAKeyPath, pubECDSAKeyPath, "JnzadsServer", "jnzads-rest", "Jnzads-rest-JWT", "ECDSA", "384", 24)
+
+  GojwtObject, err = gojweto.NewGojwetoOptions(privECDSAKeyPath, pubECDSAKeyPath, "JnzadsServer", "jnzads-rest", "Jnzads-rest-JWT", "ECDSA", "384", 24)
   fmt.Println("Example with empty secret Key and RSA/ECDSA method: ", GojwtObject, "with error: ", err)
-  //GojwtObject, err = gojweto.NewGojwetoOptions(privRSAKeyPath, pubRSAKeyPath, "JnzadsServer", "jnzads-rest", "Jnzads-rest-JWT", "RSA", "256", 24)
-  GojwtObject, err = NewGojwetoOptions(privRSAKeyPath, pubRSAKeyPath, "JnzadsServer", "jnzads-rest", "Jnzads-rest-JWT", "RSA", "256", 24)
+
+  GojwtObject, err = gojweto.NewGojwetoOptions(privRSAKeyPath, pubRSAKeyPath, "JnzadsServer", "jnzads-rest", "Jnzads-rest-JWT", "RSA", "256", 24)
   fmt.Println("Example with empty secret Key and RSA/ECDSA method: ", GojwtObject, "with error: ", err)
 }
