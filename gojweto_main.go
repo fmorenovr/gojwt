d33450f5418f2a33ef590c8fbebcafde9c105d4c
Felipe A. Moreno <fmorenovr@gmail.com>
2018-03-23 11:55:59 -0500

    Add files via upload

---
+++ gojweto_main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+  "fmt";
+  "github.com/jenazads/gojweto";
+)
+
+var (
+  privECDSAKeyPath = "asdsa"
+  pubECDSAKeyPath  = ""
+  privRSAKeyPath   = ""
+  pubRSAKeyPath    = ""
+)
+
+func main() {
+  GojwtObject, err := gojweto.NewGojweto()
+  fmt.Println("Example with Default config GoJweto Object: ", GojwtObject, "with error: ", err)
+  
+  GojwtObject, err = gojweto.NewGojwetoOptions("", "", "", "", "Jnzads-rest-JWT", "HMAC-SHA", "512", 24)
+  fmt.Println("Example with empty secret Key and HMAC method: ", GojwtObject, "with error: ", err)
+  GojwtObject, err = gojweto.NewGojwetoOptions(privECDSAKeyPath, pubECDSAKeyPath, "JnzadsServer", "jnzads-rest", "Jnzads-rest-JWT", "ECDSA", "384", 24)
+  fmt.Println("Example with empty secret Key and RSA/ECDSA method: ", GojwtObject, "with error: ", err)
+  GojwtObject, err = gojweto.NewGojwetoOptions(privRSAKeyPath, pubRSAKeyPath, "JnzadsServer", "jnzads-rest", "Jnzads-rest-JWT", "RSA", "256", 24)
+  fmt.Println("Example with empty secret Key and RSA/ECDSA method: ", GojwtObject, "with error: ", err)
+}
