1756ad413fbd2d006f298d268716d496dab8b286
Felipe A. Moreno <fmorenovr@gmail.com>
2018-03-23 11:06:45 -0500

    Update gojwetoErrors.go

--- gojwetoErrors.go
+++ gojwetoErrors.go
@@ -6,15 +6,17 @@ import (
 )
 
 var(
-  ErrInvalidEmptyToken   = errors.New("Invalid Empty Token")
-  ErrInvalidAlgorithm    = errors.New("Invalid Algorithm to create token")
-  ErrInvalidRSABytes     = errors.New("Invalid RSA len bytes Algorithm")
-  ErrInvalidECDSABytes   = errors.New("Invalid ECDSA len bytes Algorithm")
-  ErrInvalidHMACHSABytes = errors.New("Invalid HMAC-SHA len bytes Algorithm")
-  ErrInvalidToken        = errors.New("Invalid Token")
-  ErrBadFormatToken      = errors.New("Invalid Format Token")
-  ErrTokenExpired        = errors.New("Invalid Token is Expired")
-  ErrNotWorkToken        = errors.New("Invalid Token is not working")
+  ErrInvalidEmptyPrivateKey  = errors.New("Invalid Empty Path Private Key")
+  ErrInvalidEmptyPublicKey   = errors.New("Invalid Empty Path Public Key")
+  ErrInvalidEmptyToken       = errors.New("Invalid Empty Token")
+  ErrInvalidAlgorithm        = errors.New("Invalid Algorithm to create token")
+  ErrInvalidRSABytes         = errors.New("Invalid RSA len bytes Algorithm")
+  ErrInvalidECDSABytes       = errors.New("Invalid ECDSA len bytes Algorithm")
+  ErrInvalidHMACHSABytes     = errors.New("Invalid HMAC-SHA len bytes Algorithm")
+  ErrInvalidToken            = errors.New("Invalid Token")
+  ErrBadFormatToken          = errors.New("Invalid Format Token")
+  ErrTokenExpired            = errors.New("Invalid Token is Expired")
+  ErrNotWorkToken            = errors.New("Invalid Token is not working")
 )
 
 // func to evaluate Err
