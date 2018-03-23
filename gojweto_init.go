0b5399bd29f2e773360c18a0cb9f66c3106f6c43
Felipe A. Moreno <fmorenovr@gmail.com>
2018-03-23 14:01:21 -0500

    Update gojweto_init.go

--- gojweto_init.go
+++ gojweto_init.go
@@ -3,5 +3,7 @@ package gojweto
 import "fmt";
 
 func init() {
-  fmt.Println("\n \n ===> goJweto version 2.1 last update 30/03/2018 \n\n", " ==> Init the Gojweto Library for JSON Web Token developed in Golang :D", "\n\n", "===> by Jenazads.")
+  fmt.Println("\n\n====> goJweto version 2.0.1 last update 30/03/2018 \n")
+  fmt.Println("====> Init the Gojweto Library for JSON Web Token developed in Golang :D\n")
+  fmt.Println("====> by Jenazads.\n\n")
 }
