9c3f3bd4b4a9d65d0524e0a97f87f7183174f376
Jenazads <felipe.moreno.vera@gmail.com>
2018-07-26 05:00:34 -0500

    init gojwt

--- gojwt_init.go
+++ gojwt_init.go
@@ -3,7 +3,6 @@ package gojwt
 import "fmt";
 
 func init() {
-  fmt.Println("\n\n====> goJwt (GoJweto) version 2.0.1 last update 30/03/2018 \n")
-  fmt.Println("====> Init the Gojwt (GoJweto) Library for JSON Web Token developed in Golang :D\n")
-  fmt.Println("====> by Jenazads.\n\n")
+  fmt.Println("====> Init GoJWT, a JSON Web Token Library :D")
+  fmt.Println("====> by Jenazads.\n")
 }
